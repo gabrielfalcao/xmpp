2e54afa2571dce4a861790c3f05d1d09b17b43d1
Gabriel Falcão <gabriel@nacaolivre.org>
2018-04-29 00:54:50 -0300

    no unicode literals

--- .upload-release.py
+++ .upload-release.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-from __future__ import unicode_literals
 import os
 import boto
 from boto.exception import S3ResponseError
--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import unicode_literals
 from mock import patch
 from xmpp.core import ET
 from xmpp.core import generate_id
